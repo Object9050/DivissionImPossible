19477cb53198fcb4a01f9180a01d2aef7051f101
Jodie Sauer <jodie.sauer@docc.techstarter.de>
2022-12-04 20:03:18 +0100

    Abgabeversion

--- code.js
+++ code.js
@@ -1,4 +1,3 @@
-let a = [2,3,5,7,11,13,17]
 /////////////////////////////////////////////////////////////////////
 /////////////////////// Event Listener /////////////////////////////
 // Event Listener für Button "Start". Macht einen Callback auf startGame().
@@ -8,42 +7,6 @@ function startGameButtonEventListener(){
     return start;
 }
 startGameButtonEventListener();
-
-function divissionImpossible(){
-    // Eingegebene Zahl einlesen
-    let x = document.getElementById("guess").value;
-    // Die drei Dialogzeilen werden in Variablen gespeichert
-    /* let msg1 = document.getElementById("message1");
-    let msg2 = document.getElementById("message2");
-    let msg3 = document.getElementById("message3");
-    let msg4 = document.getElementById("message4");
-    let msg5 = document.getElementById("message5");
-    let msg6 = document.getElementById("message6");
-    let msg7 = document.getElementById("message7"); */
-    
-    for (let i in a){
-        if ((x % a[i] == 0) || (x % a[i] == -0)){
-            console.log ("Die Zahl ist teilbar durch " + a[i]);
-            /* msg1.innerHTML = "Die Zahl ist teilbar durch " + a[i];
-            msg2.innerHTML = "Die Zahl ist teilbar durch " + a[i];
-            msg3.innerHTML = "Die Zahl ist teilbar durch " + a[i];
-            msg4.innerHTML = "Die Zahl ist teilbar durch " + a[i];
-            msg5.innerHTML = "Die Zahl ist teilbar durch " + a[i];
-            msg6.innerHTML = "Die Zahl ist teilbar durch " + a[i];
-            msg7.innerHTML = "Die Zahl ist teilbar durch " + a[i]; */
-        }
-        else {
-            console.log ("Die Zahl ist nicht teilbar durch " + a[i]);
-            /* msg1.innerHTML = "Die Zahl ist teilbar durch " + a[i];
-            msg2.innerHTML = "Die Zahl ist teilbar durch " + a[i];
-            msg3.innerHTML = "Die Zahl ist teilbar durch " + a[i];
-            msg4.innerHTML = "Die Zahl ist teilbar durch " + a[i];
-            msg5.innerHTML = "Die Zahl ist teilbar durch " + a[i];
-            msg6.innerHTML = "Die Zahl ist teilbar durch " + a[i];
-            msg7.innerHTML = "Die Zahl ist teilbar durch " + a[i]; */
-        }
-    }
-}
 
 // Allgemeine createElement-Funktion. Nimmt den Elementtyp und ein Objekt mit Attributwerten entgegenen.
 // Ein Element mit entsprechendem Typ wird erstellt. Danach wird durch die Attribute des Objekts iteriert 
@@ -61,31 +24,38 @@ function createElement(type, attributes){
 /////////////////////// Kern-Funktionen /////////////////////////////
 // Funktion, die das Spielfeld aufbaut. Wird über den Start-Button aufgerufen.
 function startGame(){
+    // Prüfen, ob schon eine Runde gespielt wurde. Falls ja, Spielfeld löschen und neu erstellen.
+    if (document.getElementById("info1")){
+        // Alte Elemente entfernen */
+        document.getElementById("info1").remove();
+        document.getElementById("info2").remove();
+        document.getElementById("guess").remove();
+        document.getElementById("ok-btn").remove();
+        document.getElementById("info3").remove();
+        while (document.getElementById("teilbar")){
+            document.getElementById("teilbar").remove();
+        }
+        while (document.getElementById("nichtTeilbar")){
+            document.getElementById("nichtTeilbar").remove();
+        };
+        /* document.getElementById("message2").remove();
+        document.getElementById("message3").remove(); */
+    }
     /////// Spielfeld aufbauen
     // Neue Text-Elemente erstellen. Info an User.
     createElement("h3", {"innerHTML":"Wir überprüfen, ob eine beliebige Zahl<br/>durch 2, 3, 5, 7, 11, 13 oder 17 teilbar ist", "id":"info1"});
-    createElement("h3", {"innerHTML":"Gib deinen Tipp im Kästchen ein und<br/>drücke 'OK' oder 'Enter'", "id":"info2"});
+    createElement("h3", {"innerHTML":"Gib eine beliebige Zahl im Kästchen ein und<br/>drücke 'OK' oder 'Enter'", "id":"info2"});
 
     // Erstelle Input Feld mit Typ Zahlen.
     createElement("input", {"type":"number", "id": "guess"});
 
     // Erstelle OK-Button
     createElement("button", {"textContent":"OK", "id":"ok-btn"});
-
-    // Erstelle 7 Dialog-Zeilen
-    createElement("opo", {"textContent":"", "id":"message1"});
-    createElement("p", {"textContent":"", "id":"message2"});
-    createElement("p", {"textContent":"", "id":"message3"});
-    createElement("p", {"textContent":"", "id":"message4"});
-    createElement("p", {"textContent":"", "id":"message5"});
-    createElement("p", {"textContent":"", "id":"message6"});
-    createElement("p", {"textContent":"", "id":"message7"});
-
     
     /////// Event Listener
-    // Event Listener auf Ok-Button mit Callback auf compareInput().
+    // Event Listener auf Ok-Button mit Callback auf divissionImPossible().
     let compare = document.getElementById('ok-btn');
-    compare.addEventListener("click", divissionImpossible);
+    compare.addEventListener("click", divissionImPossible);
 
     // Event Listener, der beim Drücken von "Enter" einen click auf den OK-Button auslöst.
     // Dies geht nur, wenn das Input Feld aktiv ist, d.h. wenn darin eine Eingabe passiert. 
@@ -96,4 +66,22 @@ function startGame(){
             document.getElementById("ok-btn").click();
         }
     });
+}
+// Prüft, ob Zahlen ohne Rest teilbar sind und erstellt Rückmeldungen.
+function divissionImPossible(){
+    // Eingegebene Zahl einlesen
+    let x = document.getElementById("guess").value;
+    // Teiler in Array ablegen
+    let a = [2,3,5,7,11,13,17]
+    // Die eingegebene Zahl wird nacheinander durch alle Zahlen im Array geteilt. Prüfung auf Rest=0?    
+    for (let i in a){
+        if ((x % a[i] == 0) || (x % a[i] == -0)){
+            createElement("p", {"textContent":"Die Zahl ist teilbar durch " + a[i], "id":"teilbar"});
+        }
+        else {
+            createElement("p", {"textContent":"Die Zahl ist nicht teilbar durch " + a[i], "id":"nichtTeilbar"});
+        }
+    }
+    document.getElementById("ok-btn").removeEventListener("click", divissionImPossible);
+    createElement("h1", {"textContent":"Drücke auf Start für noch eine Runde", "id":"info3"});
 }
